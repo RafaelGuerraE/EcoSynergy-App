83c1323e716acf10b19ff4d57185e358c9f871af
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-04-09 09:46:55 -0300

    Initial Commit

---
+++ app/src/main/java/br/ecosynergy_app/TeamActivity.kt
@@ -0,0 +1,11 @@
+package br.ecosynergy_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TeamActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_team)
+    }
+}
