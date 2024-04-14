162d4c241ea9665ec1f3545279a5e830affeb664
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-04-14 20:56:42 -0300

    Initial Layout Changes

--- app/src/main/java/br/ecosynergy_app/LoginActivity.kt
+++ app/src/main/java/br/ecosynergy_app/LoginActivity.kt
@@ -1,11 +1,23 @@
 package br.ecosynergy_app
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val btnlogin: Button = findViewById(R.id.btnlogin)
+        val txtemail: EditText = findViewById(R.id.txtemail)
+        val txtpassword: EditText = findViewById(R.id.txtpassword)
+
+        btnlogin.setOnClickListener(){
+            val intent: Intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
