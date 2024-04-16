d67b8ac14f66df29d9b638f1522f79376a785273
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-04-16 20:25:30 -0300

    Bottom Navigation View Design

--- app/src/main/java/br/ecosynergy_app/HomeActivity.kt
+++ app/src/main/java/br/ecosynergy_app/HomeActivity.kt
@@ -2,10 +2,42 @@ package br.ecosynergy_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class HomeActivity : AppCompatActivity() {
+    //Variables
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        val bottomNavigationView: BottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        replaceFragment(Home())
+
+        bottomNavigationView.menu.findItem(R.id.home)?.isChecked = true
+
+        bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.analytics -> replaceFragment(Analytics())
+                R.id.home -> replaceFragment(Home())
+                R.id.teams -> replaceFragment(Teams())
+
+                else ->{}
+            }
+            true
+        }
+
     }
+
+    fun replaceFragment(fragment : Fragment){
+
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
+    }
+
 }
