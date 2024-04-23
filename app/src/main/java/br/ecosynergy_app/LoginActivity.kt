b9d1f2971687f2635670a239f2bb0eedfac4e9e7
Rafael <rafaelge.19@outlook.com.br>
2024-04-23 09:43:32 -0300

    Login Routine and AppBarLayout

--- app/src/main/java/br/ecosynergy_app/LoginActivity.kt
+++ app/src/main/java/br/ecosynergy_app/LoginActivity.kt
@@ -5,20 +5,41 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.startActivity
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        /*if (isLoggedIn()) {
+            startHomeActivity()
+            finish()
+            return
+        }*/
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         val btnlogin: Button = findViewById(R.id.btnlogin)
-        val txtemail: EditText = findViewById(R.id.txtemail)
+        val txtentry: EditText = findViewById(R.id.txtemail)
         val txtpassword: EditText = findViewById(R.id.txtpassword)
         val btnregister: Button = findViewById(R.id.btnregister)
 
+        val entry = txtentry.text.toString()
+        val password = txtpassword.text.toString()
+
         btnlogin.setOnClickListener(){
-            val i = Intent(this, HomeActivity::class.java)
-            startActivity(i)
+            if (authenticateUser(entry, password)) {
+                setLoggedIn(true)
+                startHomeActivity()
+
+                finish()
+            } else {
+                Toast.makeText(this, "Wrong Username/Email or Password", LENGTH_SHORT).show()
+            }
+
         }
 
         btnregister.setOnClickListener(){
@@ -26,4 +47,30 @@ class LoginActivity : AppCompatActivity() {
             startActivity(i)
         }
     }
+
+    private fun authenticateUser(entry: String, password:String): Boolean {
+        return entry == "GabrielBen" && password == "1234"
+    }
+
+    /*private fun isLoggedIn(): Boolean {
+        // Check the stored logged in status
+        // This could be checking SharedPreferences, local database, etc.
+        // Return true if the user is logged in, false otherwise
+
+    }*/
+
+    private fun setLoggedIn(loggedIn: Boolean) {
+        // Store the logged in status
+        // This could be storing in SharedPreferences, local database, etc.
+        loggedIn
+    }
+
+    private fun startHomeActivity() {
+        // Start the Home activity
+        val i = Intent(this, HomeActivity::class.java)
+        startActivity(i)
+    }
 }
+
+
+
