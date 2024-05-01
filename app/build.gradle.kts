cbf0f9d683712aa7a43d049ae2843c86f4c0f560
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-05-01 20:14:36 -0300

    LoginActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 
     implementation ("com.google.android.gms:play-services-auth:19.2.0")
+
+    implementation ("com.google.android.material:material:1.3.0-alpha03")
 }
