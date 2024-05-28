5f70c2fc2ea3a3eaccfe8ac393fcd8a1d60d3c02
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-05-28 09:46:45 -0300

    Password Error Handling

--- app/src/main/java/br/ecosynergy_app/login/LoginActivity.kt
+++ app/src/main/java/br/ecosynergy_app/login/LoginActivity.kt
@@ -3,6 +3,8 @@ package br.ecosynergy_app.login
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
@@ -15,13 +17,16 @@ import br.ecosynergy_app.RetrofitClient
 import br.ecosynergy_app.home.HomeActivity
 import br.ecosynergy_app.register.RegisterActivity
 import com.google.android.gms.common.SignInButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var txtEntry: EditText
-    private lateinit var txtPassword: EditText
+    private lateinit var txtPassword: TextInputEditText
     private lateinit var authViewModel: AuthViewModel
     private lateinit var lblReset: TextView
+    private var hasErrorShown = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -32,10 +37,12 @@ class LoginActivity : AppCompatActivity() {
             return
         }
 
-        authViewModel = ViewModelProvider(this, AuthViewModelFactory(RetrofitClient.authService)).get(AuthViewModel::class.java)
+        authViewModel = ViewModelProvider(this, AuthViewModelFactory(RetrofitClient.authService))
+            .get(AuthViewModel::class.java)
 
         txtEntry = findViewById(R.id.txtEntry)
         txtPassword = findViewById(R.id.txtPassword)
+        val passwordLayout: TextInputLayout = findViewById(R.id.passwordLayout)
         val btnLogin: Button = findViewById(R.id.btnLogin)
         val btnRegister: Button = findViewById(R.id.btnRegister)
         val btnGoogle: SignInButton = findViewById(R.id.btnGoogle)
@@ -53,14 +60,33 @@ class LoginActivity : AppCompatActivity() {
                     txtEntry.error = "Insira seu Nome de Usuário"
                 }
                 if (password.isEmpty()) {
+                    passwordLayout.endIconMode = TextInputLayout.END_ICON_NONE
                     txtPassword.error = "Insira sua senha"
+                    hasErrorShown = true
                 }
                 return@setOnClickListener
             }
-
             val loginRequest = LoginRequest(username, password)
             authViewModel.loginUser(loginRequest)
         }
+
+        txtPassword.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+                if (!s.isNullOrEmpty()) {
+                    passwordLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                    passwordLayout.error = null
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (hasErrorShown && s.isNullOrEmpty()) {
+                    passwordLayout.endIconMode = TextInputLayout.END_ICON_PASSWORD_TOGGLE
+                    hasErrorShown = false
+                }
+            }
+        })
 
         btnRegister.setOnClickListener {
             val i = Intent(this, RegisterActivity::class.java)
@@ -75,7 +101,9 @@ class LoginActivity : AppCompatActivity() {
             }.onFailure { error ->
                 error.printStackTrace()
                 Log.d("LoginActivity", "Login failed: ${error.message}")
-                showToast("Dados Incorretos")
+                txtEntry.error = "Usuário/Email ou Senha incorreto! Por favor verifique seus dados"
+                txtPassword.text = null
+                txtEntry.requestFocus()
             }
         }
 
--- app/src/main/java/br/ecosynergy_app/register/RegisterActivity.kt
+++ app/src/main/java/br/ecosynergy_app/register/RegisterActivity.kt
@@ -3,25 +3,58 @@ package br.ecosynergy_app.register
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.webkit.WebView.FindListener
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import br.ecosynergy_app.R
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class RegisterActivity : AppCompatActivity() {
+
+    private var hasErrorShown = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val btnregister: Button = findViewById(R.id.btnregister)
-        val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        val btnRegister: Button = findViewById(R.id.btnregister)
+        val btnBack : ImageButton = findViewById(R.id.btnBack)
+        val txtPassword: TextInputEditText = findViewById(R.id.txtPassword)
+        val txtConfirmPassword: TextInputEditText = findViewById(R.id.txtConfirmPassword)
+        val passwordLayout: TextInputLayout = findViewById(R.id.passwordLayout)
 
         btnBack.setOnClickListener(){
             finish()
         }
 
-        btnregister.setOnClickListener(){
-            val intent = Intent(this, RegisterActivity2::class.java)
-            startActivity(intent)
+        btnRegister.setOnClickListener(){
+
+            val password = txtPassword.text.toString()
+            val confirmPassword = txtConfirmPassword.text.toString()
+
+            if (password.isEmpty() || confirmPassword.isEmpty()) {
+                if (password.isEmpty()) {
+                    passwordLayout.endIconMode = TextInputLayout.END_ICON_NONE
+                    txtPassword.error = "Insira sua senha"
+                    hasErrorShown = true
+                }
+                if (confirmPassword.isEmpty()) {
+                    passwordLayout.endIconMode = TextInputLayout.END_ICON_NONE
+                    txtPassword.error = "Insira sua senha"
+                    hasErrorShown = true
+                }
+                return@setOnClickListener
+            }
+            if (txtPassword.text.toString() != txtConfirmPassword.text.toString()){
+                txtConfirmPassword.error = "As senhas não se correspondem"
+            }
+            else
+            {
+                val i = Intent(this, RegisterActivity2::class.java)
+                startActivity(i)
+            }
+
         }
     }
 }
