3c57f6d460997b86f4f036315803853d65deb85e
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-06-26 19:18:49 -0300

    UserSettings and NotificationsActivity

--- app/src/main/java/br/ecosynergy_app/home/UserSettingsActivity.kt
+++ app/src/main/java/br/ecosynergy_app/home/UserSettingsActivity.kt
@@ -1,6 +1,7 @@
 package br.ecosynergy_app.home
 
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import br.ecosynergy_app.R
@@ -11,6 +12,12 @@ class UserSettingsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_usersettings)
 
         val btnBack = findViewById<ImageButton>(R.id.btnBack)
+        val btnEditUsername = findViewById<ImageButton>(R.id.btnEditUsername)
+        val btnEditFullname = findViewById<ImageButton>(R.id.btnEditFullname)
+        val btnEditGender = findViewById<ImageButton>(R.id.btnEditGender)
+        val btnEditNationality = findViewById<ImageButton>(R.id.btnEditNationality)
+        val btnEditPassword = findViewById<ImageButton>(R.id.btnEditPassword)
+
 
         btnBack.setOnClickListener(){
             finish()
