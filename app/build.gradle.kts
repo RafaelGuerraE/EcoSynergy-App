b8825ac54f226e289c1af35f3b4e35bca9e870ee
Rafael Guerra <rafaelge.19@outlook.com.br>
2024-07-13 12:04:18 -0300

    ShimmerHomeFragment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,19 +53,19 @@ dependencies {
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
 
-    implementation(platform("com.google.firebase:firebase-bom:33.1.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.1.1"))
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.android.gms:play-services-auth:21.2.0")
 
     implementation("com.google.mlkit:vision-internal-vkp:18.2.2")
     implementation("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.fragment:fragment-ktx:1.8.0")
-    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.1")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.8.3")
     implementation("com.google.firebase:firebase-auth-ktx:23.0.0")
 
     testImplementation("junit:junit:4.13.2")
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
@@ -74,7 +74,9 @@ dependencies {
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
     implementation ("androidx.activity:activity-ktx:1.9.0")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.3")
 
     implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+
+    implementation("com.facebook.shimmer:shimmer:0.5.0")
 }
--- app/src/main/java/br/ecosynergy_app/home/homefragments/Home.kt
+++ app/src/main/java/br/ecosynergy_app/home/homefragments/Home.kt
@@ -7,19 +7,19 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ProgressBar
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import br.ecosynergy_app.R
 import br.ecosynergy_app.home.UserViewModel
+import com.facebook.shimmer.ShimmerFrameLayout
 
 class Home : Fragment() {
 
     private lateinit var lblFirstname: TextView
-    private lateinit var progressBar: ProgressBar
     private lateinit var swipeRefresh: SwipeRefreshLayout
+    private lateinit var shimmerViewContainer: ShimmerFrameLayout
     private val userViewModel: UserViewModel by viewModels()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
@@ -27,11 +27,16 @@ class Home : Fragment() {
 
         swipeRefresh = view.findViewById(R.id.swipeRefresh)
         lblFirstname = view.findViewById(R.id.lblFirstname)
-        progressBar = view.findViewById(R.id.progressBar)
+        shimmerViewContainer = view.findViewById(R.id.shimmerViewContainer)
 
         userViewModel.user.observe(viewLifecycleOwner) { result ->
-            progressBar.visibility = View.GONE
-            lblFirstname.visibility = View.VISIBLE
+            shimmerViewContainer.animate().alpha(0f).setDuration(300).withEndAction {
+                shimmerViewContainer.stopShimmer()
+                shimmerViewContainer.visibility = View.GONE
+                lblFirstname.visibility = View.VISIBLE
+                lblFirstname.animate().alpha(1f).setDuration(300)
+            }
+
             result.onSuccess { user ->
                 val firstName = user.fullName.split(" ").firstOrNull()
                 lblFirstname.text = "$firstName!"
@@ -48,8 +53,11 @@ class Home : Fragment() {
     }
 
     private fun fetchUserData() {
-        progressBar.visibility = View.VISIBLE
         lblFirstname.visibility = View.GONE
+        lblFirstname.alpha = 0f
+        shimmerViewContainer.visibility = View.VISIBLE
+        shimmerViewContainer.startShimmer()
+        shimmerViewContainer.alpha = 1f
 
         val sharedPreferences: SharedPreferences = requireActivity().getSharedPreferences("login_prefs", Context.MODE_PRIVATE)
         val username = sharedPreferences.getString("username", null)
@@ -58,8 +66,12 @@ class Home : Fragment() {
         if (username != null && token != null) {
             userViewModel.fetchUserData(username, token)
         } else {
-            progressBar.visibility = View.GONE
-            lblFirstname.visibility = View.VISIBLE
+            shimmerViewContainer.animate().alpha(0f).setDuration(300).withEndAction {
+                shimmerViewContainer.stopShimmer()
+                shimmerViewContainer.visibility = View.GONE
+                lblFirstname.visibility = View.VISIBLE
+                lblFirstname.animate().alpha(1f).setDuration(300)
+            }
             lblFirstname.text = ""
         }
     }
